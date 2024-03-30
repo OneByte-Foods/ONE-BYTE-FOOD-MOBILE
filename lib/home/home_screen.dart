e84c681516e8242d49d11542a2c8b5283fff41da
Dev00kushal <devkushal00@gmail.com>
2024-03-30 13:08:37 +0545

    🔥 Refactor home screen to use a bottom navigation bar

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,10 +1,125 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+  final List<Widget> _pages = [
+    HomePage(),
+    ShopPage(),
+    FavoritesPage(),
+    SettingsPage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.green[50],
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+        backgroundColor: Colors.green,
+        foregroundColor: Colors.yellow,
+        elevation: 0,
+      ),
+      bottomNavigationBar: BottomAppBar(
+        notchMargin: 5.0,
+        shape: CircularNotchedRectangle(),
+        color: Colors.black87,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            buildBottomNavItem(Icons.home, "Home", 0),
+            buildBottomNavItem(Icons.shopping_cart, "Shop", 1),
+            buildBottomNavItem(Icons.favorite, "Fav", 2),
+            buildBottomNavItem(Icons.settings, "Setting", 3),
+          ],
+        ),
+      ),
+      body: _pages[_selectedIndex],
+    );
+  }
+
+  Widget buildBottomNavItem(IconData icon, String label, int index) {
+    return InkWell(
+      onTap: () {
+        _onItemTapped(index);
+      },
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(
+            icon,
+            color: Colors.white,
+          ),
+          SizedBox(height: 4),
+          Text(
+            label,
+            style: TextStyle(color: Colors.white),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red, // Example color, replace with your content
+      child: Center(
+        child: Text('Home Page'),
+      ),
+    );
+  }
+}
+
+class ShopPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue, // Example color, replace with your content
+      child: Center(
+        child: Text('Shop Page'),
+      ),
+    );
+  }
+}
+
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.green, // Example color, replace with your content
+      child: Center(
+        child: Text('Favorites Page'),
+      ),
+    );
+  }
+}
+
+class SettingsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.orange,
+      child: Center(
+        child: Text('Settings Page'),
+      ),
+    );
   }
 }
